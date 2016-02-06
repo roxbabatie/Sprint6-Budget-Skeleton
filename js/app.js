0dc047007c94dddeaf701074575df83c3ba42631
Roxana Babatie <roxana.babatie@gmail.com>
2016-02-06 13:33:15 +0200

    routes added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
-var app = angular.module('budgetingApp', []);
+(function() {
+    var app = angular.module('budgetingApp', ['ngRoute']);
+
+    var pages = [
+        {name: 'Balance', url: ''},
+        {name: 'Income', url: 'income'},
+        {name: 'Expense', url: 'expense'}
+    ];
+
+    //configure angular app
+    app.config(function($locationProvider, $routeProvider) {
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+
+        angular.forEach(pages, function(page) {
+            console.log('/' + page.url + '.html');
+            $routeProvider.when('/' + page.url, {
+                templateUrl: 'pages/' + (!page.url ? 'balance' : page.url) + '.html',
+            })
+        });
+
+        $routeProvider.otherwise({
+            templateUrl: 'pages/balance.html',
+        });
+    });
+
+
+})();
