62be6a9bc2ff598087083c020357a9439d50b651
Roxana Babatie <roxana.babatie@gmail.com>
2016-02-06 22:14:08 +0200

    added services/factory and BalanceCtrl.Table is being populated on balance page with data from server

--- js/app.js
+++ js/app.js
@@ -25,6 +25,86 @@
             templateUrl: 'pages/balance.html',
         });
     });
+    //controllers
+
+    app.controller('BalanceCtrl', function($scope, TransactionStore) {
+        $scope.transactions = [];
+
+        TransactionStore.getTransactionsInMonth(moment().format('YYYY-MM')).then(function(items) {
+            $scope.transactions = items;
+        });
+    });
+
+    //create services
+    app.factory('TransactionStore', function($http, $q) {
+        return (function() {
+            var URL = 'http://server.godev.ro:8080/api/roxanab/transactions';
+
+            var getTransactionsInMonth = function(month) {
+                return $q(function(resolve, reject) {
+                    $http({url: URL + '?month=' + month})
+                        .then(
+                            function(xhr) {
+                                if (xhr.status == 200) {
+                                    resolve(xhr.data);
+                                } else {
+                                    reject();
+                                }
+                            },
+                            reject
+                        );
+                });
+            };
+
+            var add = function(data) {
+                return $q(function(resolve, reject) {
+                    $http({
+                        url: URL,
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        data: JSON.stringify(data)
+                    })
+                        .then(
+                            function(xhr) {
+                                if (xhr.status == 201) {
+                                    resolve(xhr.data);
+                                } else {
+                                    reject();
+                                }
+                            },
+                            reject
+                        );
+                });
+            };
+
+            var del = function(id) {
+                return $q(function(resolve, reject) {
+                    $http({
+                        url: URL + '/' + id,
+                        method: 'DELETE'
+                    })
+                        .then(
+                            function(xhr) {
+                                if (xhr.status == 204) {
+                                    resolve();
+                                } else {
+                                    reject();
+                                }
+                            },
+                            reject
+                        );
+                });
+            };
+
+            return {
+                getTransactionsInMonth: getTransactionsInMonth,
+                add: add,
+                delete: del
+            };
+        })();
+    });
 
 
 })();
